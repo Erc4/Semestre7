6171177714fa3d574d0d972e8f4887f088effca5
Erc4 <ericamezcua3@gmail.com>
2024-09-29 20:38:18 -0700

    Calculo de resultados automatico

--- models.py
+++ models.py
@@ -1,8 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 db = SQLAlchemy()
 
+
+class classalimentos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    porcion = db.Column(db.Float, nullable=False)  # cantidad en gramos o unidades
+    tipo_porcion = db.Column(db.String(50), nullable=False)  # 'gramos' o 'unidad'
+    proteinas = db.Column(db.Float, nullable=False)
+    carbohidratos = db.Column(db.Float, nullable=False)
+    grasas = db.Column(db.Float, nullable=False)
+    calorias = db.Column(db.Float, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'nombre': self.nombre,
+            'porcion': self.porcion,
+            'tipo_porcion': self.tipo_porcion,
+            'proteinas': self.proteinas,
+            'carbohidratos': self.carbohidratos,
+            'grasas': self.grasas,
+            'calorias': self.calorias
+        }
+    
 class classusuarios(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     apellidopaterno = db.Column(db.String(255), nullable=False)
@@ -15,12 +39,14 @@ class classusuarios(db.Model):
     peso = db.Column(db.Float, nullable=False)
     estatura = db.Column(db.Integer, nullable=False)
     edad = db.Column(db.Integer, nullable=False)
+    sexo = db.Column(db.String(1), nullable=False)
     actividad = db.Column(db.String(255), nullable=False)
     metabolismobasal = db.Column(db.Integer, nullable=False)
     imc = db.Column(db.Float, nullable=False)
     requerimentoagua = db.Column(db.Integer, nullable=False)
     requerimientocalorico = db.Column(db.Integer, nullable=False)
     objetivo = db.Column(db.String(255), nullable=False)
+
 
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
@@ -40,6 +66,7 @@ class classusuarios(db.Model):
             'peso': self.peso,
             'estatura': self.estatura,
             'edad': self.edad,
+            'sexo': self.sexo,
             'actividad': self.actividad,
             'metabolismobasal': self.metabolismobasal,
             'imc': self.imc,
@@ -48,45 +75,3 @@ class classusuarios(db.Model):
             'objetivo': self.objetivo
         }
     
-class classalimentos(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(255), nullable=False)
-    porcion = db.Column(db.Integer, nullable=False)
-    proteinas = db.Column(db.Float, nullable=False)
-    carbohidratos = db.Column(db.Float, nullable=False)
-    grasas = db.Column(db.Float, nullable=False)
-    calorias = db.Column(db.Float, nullable=False)
-
-    def to_dict(self):
-        return{
-            'id': self.id,
-            'nombre': self.nombre,
-            'porcion': self.porcion,
-            'proteinas': self.proteinas,
-            'carbohidratos': self.carbohidratos,
-            'grasas': self.grasas,
-            'calorias': self.calorias 
-        }
-    
-
-class classmedicos(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    apellidopaterno = db.Column(db.String(255), nullable=False)
-    apellidomaterno = db.Column(db.String(255), nullable=False)
-    nombre = db.Column(db.String(255), nullable=False)
-    rol = db.Column(db.Integer, nullable = False)
-    usuario = db.Column (db.String(255), nullable=False)
-    correo = db.Column(db.String(255), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
-
-    def to_dict(self):
-        return{
-            'id': self.id,
-            'apellidopaterno': self.apellidopaterno,
-            'apellidomaterno': self.apellidomaterno,
-            'nombre': self.nombre,
-            'rol' : self.rol,
-            'usuario': self.usuario,
-            'correo': self.correo
-        }
-
--- routes.py
+++ routes.py
@@ -63,7 +63,44 @@ def get_user(id):
 @usuarios_bp.route('/usuarios', methods=['POST'])
 #@token_required
 def create_user():
+
     data = request.json
+    if not data:  # Verifica que se recibió datos
+        return jsonify({"error": "No se recibieron datos"}), 400
+
+    #Calcular imc
+    alturaMetros = data['estatura'] / (100)
+    imc = data['peso'] / (alturaMetros ** 2)
+
+    #Calcular metabolismo basal
+    if data['sexo'] == 'm' or 'M':
+        metabolismobasal = 88.362 + (13.97 * data['peso']) + (4.799 * data['estatura']) - (5.677 * data['edad'])
+    else:
+        metabolismobasal = 447.593 + (9.247 * data['peso']) + (3.098 * data['estatura']) - (4.330 * data['edad'])
+
+    #Calcular requerimiento de agua.
+    requerimientoagua = data['peso'] * 35
+    
+    #Calcular valor numérico de factor de avtividad física
+    if data['actividad'] == "baja":
+        fap = 1.2
+    elif data['actividad'] == "moderada":
+        fap = 1.55
+    elif data['actividad'] == "alta":
+        fap = 1.9
+    else:
+        return jsonify({"error": "Nivel de actividad no válido"})
+    
+    # Calcular Requerimiento Calórico según el objetivo
+    if data['objetivo'] == 'mantener':
+        requerimientocalorico = metabolismobasal * fap
+    elif data['objetivo'] == 'aumentar':
+        requerimientocalorico = metabolismobasal * fap * 1.15
+    elif data['objetivo'] == 'disminuir':
+        requerimientocalorico = metabolismobasal * fap * 1.85
+
+    
+    
     new_user = classusuarios(
         apellidopaterno=data['apellidopaterno'],
         apellidomaterno=data['apellidomaterno'],
@@ -74,10 +111,12 @@ def create_user():
         peso=data['peso'],
         estatura=data['estatura'],
         edad=data['edad'],
+        sexo=data['sexo'],
         actividad=data['actividad'],
-        metabolismobasal=data['metabolismobasal'],
-        imc=data['imc'],
-        requerimentoagua=data['requerimentoagua'],
+        metabolismobasal=metabolismobasal,
+        imc=imc,
+        requerimentoagua=requerimientoagua,
+        requerimientocalorico = requerimientocalorico,
         objetivo=data['objetivo']
     )
     new_user.set_password(data['password'])
@@ -90,14 +129,56 @@ def create_user():
 def update_userpersonaldata(id):
     user = classusuarios.query.get_or_404(id)
     data = request.json
+
+    if not data:  # Verifica que se recibió datos
+        return jsonify({"error": "No se recibieron datos"}), 400
+
     user.apellidopaterno = data['apellidopaterno']
     user.apellidomaterno = data['apellidomaterno']
     user.nombre = data['nombre']
     user.peso = data['peso']
     user.estatura = data['estatura']
     user.edad = data['edad']
+    user.sexo=data['sexo']
     user.actividad = data['actividad']
     user.objetivo = data['objetivo']
+
+    # Recalcular IMC
+    altura_metros = user.estatura / 100  # Convertir altura a metros
+    user.imc = user.peso / (altura_metros ** 2)
+
+    # Recalcular Metabolismo Basal
+    if user.sexo == 'm' or 'M':
+        user.metabolismobasal = 88.362 + (13.397 * user.peso) + (4.799 * user.estatura) - (5.677 * user.edad)
+    else:
+        user.metabolismobasal = 447.593 + (9.247 * user.peso) + (3.098 * user.estatura) - (4.330 * user.edad)
+
+    # Recalcular Requerimiento de Agua
+    user.requerimentoagua = user.peso * 35
+
+    # Mapear el nivel de actividad física a un factor numérico
+    if user.actividad == 'baja':
+        actividad_factor = 1.2
+    elif user.actividad == 'moderada':
+        actividad_factor = 1.55
+    elif user.actividad == 'alta':
+        actividad_factor = 1.9
+    else:
+        return jsonify({"error": "Nivel de actividad no válido"}), 400
+
+    # Recalcular Requerimiento Calórico según el objetivo
+    if data['objetivo'] == 'mantener':
+        user.requerimientocalorico = user.metabolismobasal * actividad_factor
+    elif data['objetivo'] == 'aumentar':
+        user.requerimientocalorico = user.metabolismobasal * actividad_factor * 1.15
+    elif data['objetivo'] == 'disminuir':
+        user.requerimientocalorico = user.metabolismobasal * actividad_factor * 0.85
+    else:
+        return jsonify({"error": "Objetivo no válido"}), 400
+
+    # Actualizar el campo de objetivo
+    user.objetivo = data['objetivo']
+
     db.session.commit()
     return jsonify(user.to_dict())
 
@@ -136,10 +217,11 @@ def get_alimento(id):
 
 @alimentos_bp.route('/alimentos', methods = ['POST'])
 def crear_alimento():
-    data = request.json()
+    data = request.json
     new_alimento = classalimentos(
         nombre=data['nombre'],
         porcion=data['porcion'],
+        tipo_porcion=data['tipo_porcion'],
         proteinas=data['proteinas'],
         carbohidratos=data['carbohidratos'],
         grasas=data['grasas'],
